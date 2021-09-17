7c957da7cc24791d3ab4be154fc696ff7623c398
Sai Sharan <saisharan2565@gmail.com>
2021-09-17 19:56:36 +0530

    custom input

---
+++ custom_input/custom_input.js
@@ -0,0 +1,25 @@
+import fs from 'fs';
+import readline from 'readline';
+import { configureMachine } from './configuremachine.js';
+
+var lineReader = readline.createInterface({
+    input: fs.createReadStream('./input.txt')
+  });
+let serverInfos = [];
+  lineReader.on('line', function (line) {
+    serverInfos.push(line.split(','));  
+  });
+  lineReader.on('close', function(){
+        exec();
+  });
+
+function exec(){
+    let servers = [];
+    serverInfos.forEach((s) => {
+        servers.push(configureMachine({"name": s[0],"ip":s[1],"neighbours":s.splice(2)}));
+    });
+        
+    servers.forEach((s) => {
+        s.scanNetwork();
+    });
+}

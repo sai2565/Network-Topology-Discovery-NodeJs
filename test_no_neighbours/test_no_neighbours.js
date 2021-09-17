41cf9330768b5c6015b11cfc1dc26d780596ee11
Sai Sharan <saisharan2565@gmail.com>
2021-09-17 20:23:15 +0530

    test no neighbours

---
+++ test_no_neighbours/test_no_neighbours.js
@@ -0,0 +1,6 @@
+import { configureMachine } from '../configuremachine.js';
+
+const A = configureMachine({ "name": "A", "ip": "9000", "neighbours": [] });
+//scan network function would be triggered by different machines simultaneously
+//here i am triggering for a random machine
+A.scanNetwork();

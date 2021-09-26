6e6efc4be7f8fb24fa75055a6b54f3cc000e5ba6
Sai Sharan <saisharan2565@gmail.com>
2021-09-26 08:16:06 +0530

    Delete test_2_machines.js

--- test_2_machines.js
+++
@@ -1,9 +0,0 @@
-import { configureMachine } from './configuremachine.js';
-
-const A = configureMachine({ "name": "A", "ip": "9000", "neighbours": [9001] });
-const B = configureMachine({ "name": "B", "ip": "9001", "neighbours": [9000] });
-
-//scan network function would be triggered by different machines simultaneously
-//here i am triggering for a random machine
-A.scanNetwork();
-B.scanNetwork();
